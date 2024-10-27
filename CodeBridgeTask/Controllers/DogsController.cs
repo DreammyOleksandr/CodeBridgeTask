0abc01745bd174cabbb1fbcc26c92acd992621b0
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-27 21:07:04 +0200

    feat: adding automapper

--- CodeBridgeTask/Controllers/DogsController.cs
+++ CodeBridgeTask/Controllers/DogsController.cs
@@ -1,3 +1,4 @@
+using AutoMapper;
 using CodeBridgeTask.BusinessLogic.Managers.DogManager;
 using CodeBridgeTask.DataAccess.Models;
 using CodeBridgeTask.DataAccess.Repositories.DogRepository;
@@ -8,9 +9,11 @@ namespace CodeBridgeTask.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DogsController(IDogManager dogManager) : ControllerBase
+    public class DogsController(IDogManager dogManager, IMapper mapper) : ControllerBase
     {
         private readonly IDogManager _dogManager = dogManager;
+        private readonly IMapper _mapper = mapper;
+        
         [HttpGet]
         [ProducesResponseType(StatusCodes.Status200OK)]
         public async Task<ActionResult<IEnumerable<Dog>>> GetAll(QueryParams queryParams)
@@ -27,11 +30,22 @@ namespace CodeBridgeTask.Controllers
         }
 
         [HttpPost]
-        public void Post([FromBody] DogDTO dogDto)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<DogDTO>> Post([FromBody] DogDTO dogDto)
         {
             try
             {
+                if(dogDto is null) 
+                    return BadRequest();
                 
+                Dog dog = _mapper.Map<Dog>(dogDto);
+                
+                if (dog.Id != 0) 
+                    return StatusCode(StatusCodes.Status500InternalServerError);
+                
+                return Ok(await _dogManager.Create(dog));
             }
             catch (Exception e)
             {
---
+++ CodeBridgeTask/MappingProfiles/AutoMapperProfile.cs
@@ -0,0 +1,13 @@
+using AutoMapper;
+using CodeBridgeTask.DataAccess.Models;
+using CodeBridgeTask.DTOs;
+
+namespace CodeBridgeTask.MappingProfiles;
+
+public class AutoMapperProfile : Profile
+{
+    public AutoMapperProfile()
+    {
+        CreateMap<DogDTO, Dog>().ReverseMap();
+    }
+}
