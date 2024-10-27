deda46a846eb56369e6c93eb6bd001f667791f50
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-27 21:13:44 +0200

    upd: ping action

--- CodeBridgeTask/Controllers/NetworkingController.cs
+++ CodeBridgeTask/Controllers/NetworkingController.cs
@@ -9,6 +9,17 @@ namespace CodeBridgeTask.Controllers
         [HttpGet]
         [Route("ping")]
         [ProducesResponseType(StatusCodes.Status200OK)]
-        public ActionResult<string> Get() => Ok("Dogshouseservice.Version1.0.1");
+        public ActionResult<string> Get()
+        {
+            try
+            {
+                return Ok("Dogshouseservice.Version1.0.1");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return StatusCode(500, new { message = "An unexpected error occurred. Please try again later." });
+            }
+        }
     }
 }
