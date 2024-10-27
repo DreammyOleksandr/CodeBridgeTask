f978c705e0c8690532b1822bc453a0ca179161f9
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-27 21:38:43 +0200

    upd: dog attributes

--- CodeBridgeTask.DataAccess/Models/Dog.cs
+++ CodeBridgeTask.DataAccess/Models/Dog.cs
@@ -6,12 +6,8 @@ public class Dog
 {
     [Key]
     public int Id { get; init; }
-    [Required(ErrorMessage = "Please enter the name of the dog")]
     public string Name { get; init; }
-    [Required(ErrorMessage = "Please enter the breed of the dog")]
     public string Color { get; init; }
-    [Required(ErrorMessage = "Please enter the age of the dog")]
     public float TailLength { get; init; }
-    [Required(ErrorMessage = "Please enter the height of the dog")]
     public float Weight { get; init; }
 }
--- CodeBridgeTask/DTOs/DogDTO.cs
+++ CodeBridgeTask/DTOs/DogDTO.cs
@@ -1,3 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace CodeBridgeTask.DTOs;
 
-public record DogDTO(string Name, string Color, float TailLength, float Weight);
+public class DogDTO
+{
+    [Required(ErrorMessage = "Please enter the name of the dog")]
+    public string Name { get; set; }
+    [Required(ErrorMessage = "Please enter the color of the dog")]
+    public string Color { get; set; }
+    [Required(ErrorMessage = "Please enter the tail length of the dog")]
+    public float TailLength { get; set; }
+    [Required(ErrorMessage = "Please enter the weight of the dog")]
+    public float Weight { get; set; }
+}
