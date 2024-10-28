649ec29cb36cf01c387d758680752e5183a4bb68
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-28 15:18:59 +0200

    upd: custom exeptions catching

--- CodeBridgeTask/Controllers/DogsController.cs
+++ CodeBridgeTask/Controllers/DogsController.cs
@@ -23,28 +23,40 @@ namespace CodeBridgeTask.Controllers
                 QueryParams queryParams = _mapper.Map<QueryParams>(queryParamsDto);
                 return Ok(_mapper.Map<IEnumerable<DogDTO>>(await _dogManager.GetRangeAsync(queryParams)));
             }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e);
+                return BadRequest(e.Message);
+            }
             catch (Exception e)
             {
                 Console.WriteLine(e);
-                return StatusCode(500, new { message = "An unexpected error occurred. Please try again later." });
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new { message = "An unexpected error occurred. Please try again later." });
             }
         }
 
         [HttpPost]
-        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status500InternalServerError)]
-        public async Task<ActionResult<DogDTO>> Create(DogDTO dogDto)
+        public async Task<ActionResult<DogDTO>> Create(CreateDogDTO createDogDto)
         {
             try
             {
-                await _dogManager.CreateAsync(_mapper.Map<Dog>(dogDto));
-                return Ok(dogDto);
+                await _dogManager.CreateAsync(_mapper.Map<Dog>(createDogDto));
+                return StatusCode(StatusCodes.Status201Created, createDogDto);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine(e);
+                return BadRequest(e.Message);
             }
             catch (Exception e)
             {
                 Console.WriteLine(e);
-                return StatusCode(500, new { message = "An unexpected error occurred. Please try again later." });
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    new { message = "An unexpected error occurred. Please try again later." });
             }
         }
     }
