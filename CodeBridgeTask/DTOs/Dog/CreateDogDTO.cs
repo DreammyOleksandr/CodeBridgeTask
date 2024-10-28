1c1b4ca5d0801c8d7be727afa481e8583a16bf1d
Bondarenko Oleksandr <albom2004q@gmail.com>
2024-10-28 15:53:23 +0200

    fix: changing JSON prop name for CreateDogDTO

--- CodeBridgeTask/DTOs/Dog/CreateDogDTO.cs
+++ CodeBridgeTask/DTOs/Dog/CreateDogDTO.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace CodeBridgeTask.DTOs;
 
@@ -8,7 +10,7 @@ public class CreateDogDTO
     public string Name { get; set; }
     [Required(ErrorMessage = "Please enter the color of the dog")]
     public string Color { get; set; }
-    [Required(ErrorMessage = "Please enter the tail length of the dog")]
+    [Required(ErrorMessage = "Please enter the tail length of the dog"), JsonPropertyName("tail_length")]
     public float TailLength { get; set; }
     [Required(ErrorMessage = "Please enter the weight of the dog")]
     public float Weight { get; set; }
